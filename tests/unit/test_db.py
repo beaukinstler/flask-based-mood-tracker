2c65a8088fb72f5581a0428752ddd08498b785d1
beau kinstler <beaukinstler@gmail.com>
2023-06-22 14:57:37 -0700

    WIP

--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -31,8 +31,9 @@ def client(test_app):
     with test_app.test_client() as client:
         yield client
 
+
 @pytest.mark.unit
-def test_database(test_app, client):
+def test_database(test_app):
     """
     GIVEN a Flask application configured for in this test file for testing via a fixture
     WHEN creating a Mood instance in the app context and commiting to datbase with "happy" as the data
@@ -51,10 +52,39 @@ def test_database(test_app, client):
         assert mood.description == 'happy'
 
     # Roll back the changes to the database
-    with test_app.app_context():
-        db.session.rollback()
+    # with test_app.app_context():
+    #     db.session.rollback()
 
     # Make assertions to verify the database rollback
+
+
+@pytest.mark.unit
+def test_model_update(test_app):
+    """
+    GIVEN a Flask application configured for in this test file for testing via a fixture
+    WHEN creating a Mood instance in the app context and commiting to datbase with "happy" as the data
+    THEN the response of the query of the database will have that matching data in it's 'description'
+    """
+
+    with test_app.app_context():
+        # Example: Insert a user into the database
+        mood_data = 'happy'
+        mood = Mood(mood_data)
+        db.session.add(mood)
+        db.session.commit()
+
+        # Make assertions to verify the data insertion
+        mood = Mood.query.get(1)
+        assert mood.description == 'happy'
+
+        # use Model's update
+        mood.update('sad')
+        mood = Mood.query.get(1)
+        assert mood.description == 'sad'
+
+    # Make assertions to verify the database rollback
+
+
 @pytest.mark.fixture
 @pytest.mark.unit
 def test_database_with_confest_fixture(app):
