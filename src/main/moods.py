1c785d0798019ae3c24569b7129ab57c62f99a2f
Beau <git@beaukinstler.info>
2023-07-27 20:23:16 -0500

    add form and support for mood

--- src/main/moods.py
+++ src/main/moods.py
@@ -1,13 +1,56 @@
-from flask import Blueprint, jsonify, abort, request
-from ..models import Mood
+from flask import Blueprint, jsonify, abort, url_for, flash
+from flask import redirect as flask_redirect, request as flask_request, render_template as flask_render_template
+from ..models import Mood, UserMoodLog, User
 from sqlalchemy.exc import IntegrityError
 from src import db
+from flask_wtf import FlaskForm
+from wtforms import HiddenField
+from wtforms.validators import DataRequired
+from flask_login import login_required, current_user
 
 bp = Blueprint("moods", __name__, url_prefix="/moods")
 
 
-@bp.route("", methods=['GET'])
+class MoodForm(FlaskForm):
+    mood = HiddenField('mood', validators=[DataRequired()])
+
+
+
+@bp.route("", methods=['GET','POST'])
+@login_required
 def index():
+    form = MoodForm()
+
+    if flask_request.method == 'POST':
+        if form.validate():
+            pass
+        else:
+            pass
+        id = int(flask_request.form['button'])
+        token = str(flask_request.form['token'])
+        mood = Mood.query.all()
+        moods = [ m for m in mood if m.id == id ]
+        if moods:
+            mood = moods[0]
+            mood_description = mood.description
+            response_data = {
+                'message': f"Button {mood_description} pressed!",
+                'token': f"{token}"
+            }
+            log = UserMoodLog()
+            log.mood = mood
+            current_user.moods.append(log)
+            # db.session.add(log)
+            db.session.commit()
+            flash(f'Logged your {mood_description} mood...')
+            return flask_redirect(url_for('users.me'))
+
+    return flask_render_template('mood.html', form=form)
+
+
+@bp.route("/all", methods=['GET'])
+@login_required
+def all():
     moods = Mood.query.all()
     result = []
     for mood in moods:
@@ -16,10 +59,11 @@ def index():
 
 
 @bp.route("/create", methods=['POST'])
+@login_required
 def create():
-    if 'description' not in request.json:
+    if 'description' not in flask_request.json:
         return abort(400)
-    mood = Mood(request.json['description'])
+    mood = Mood(flask_request.json['description'])
     return jsonify(mood.serialize())
 
 
@@ -39,16 +83,17 @@ def delete(id: int):
 
 
 @bp.route("/<int:id>", methods=['PUT', 'PATCH'])
+@login_required
 def update(id: int):
     """
     update a mood name
     """
 
-    if 'description' not in request.json:
+    if 'description' not in flask_request.json:
         return abort(400)
     mood = Mood.query.get_or_404(id)
 
-    description = request.json['description']
+    description = flask_request.json['description']
 
     if description is not None and description != '':
         mood.description = description
