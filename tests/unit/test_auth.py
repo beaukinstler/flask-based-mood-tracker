05d7397d401a63387be85ab876672df4ee701878
Beau <git@beaukinstler.info>
2023-08-02 22:29:53 -0500

    try out class tests

--- tests/unit/test_auth.py
+++ tests/unit/test_auth.py
@@ -4,98 +4,108 @@ from src.models import db, User
 from flask_login import logout_user, login_user, current_user
 
 
-@pytest.mark.auth
-@pytest.mark.users
-@pytest.mark.unit
-def test_flask_login_password(testclient):
+class TestUserAuth:
     """
-    GIVEN a Flask application configured for in this test file for testing via a fixture
-    WHEN using the word 'password' as the password
-    THEN the verify_password function will succeed, and the actual password will be a hashed string
+    Test the User model with authentication
+
     """
 
-    # Example: Insert a user into the database
-    user_email = 'user@example.com'
-    user_password = 'password'
-    user = User(email=user_email, password=user_password)
-    assert user.password != 'password'
-    assert user.verify_password('password')
-    assert len(user.password) > 100
-    pre_db_password = user.password
+    @pytest.mark.auth
+    @pytest.mark.users
+    @pytest.mark.unit
+    def test_user_login_password(self, testclient):
+        """
+        GIVEN a User model
+        WHEN setting the password as 'password'
+        THEN the verify_password function should succeed, and the actual password will be a hashed string
+        """
 
-    db.session.add(user)
-    db.session.commit()
-    users = db.session.query(User).all()
-    assert users[0].verify_password('password')
-    assert pre_db_password == users[0].password
+        # Example: Insert a user into the database
+        user_email = 'user@example.com'
+        user_password = 'password'
+        user = User(email=user_email, password=user_password)
+        assert user.password != 'password'
+        assert user.verify_password('password')
+        assert len(user.password) > 100
+        pre_db_password = user.password
 
-
-@pytest.mark.auth
-@pytest.mark.users
-@pytest.mark.unit
-def test_flask_login_properties():
-    """
-    GIVEN a Flask application configured for in this test file for testing via a fixture
-    WHEN creating a User instance in the app context and commiting to datbase with email and password
-    THEN the new user object will have and id property == 1
-    """
-
-    # Example: Insert a user into the database
-    user_email = 'user@example.com'
-    user_password = 'password'
-    user = User(email=user_email, password=user_password)
-
-    assert user.is_authenticated == True
-    assert user.is_active == True
-    assert user.is_anonymous == False
-    # for the flask_login, i think they want the username
-    assert user.get_id() == 'user@example.com'
+        db.session.add(user)
+        db.session.commit()
+        users = db.session.query(User).all()
+        assert users[0].verify_password('password')
+        assert pre_db_password == users[0].password
 
 
-@pytest.mark.auth
-@pytest.mark.users
-@pytest.mark.unit
-def test_flask_login_properties_database_committed(testclient):
-    """
-    GIVEN a Flask application configured for in this test file for testing via a fixture
-    WHEN creating a User instance in the app context and commiting to datbase with email and password
-    THEN the new user object will have and id property == 1
-    """
+    @pytest.mark.auth
+    @pytest.mark.users
+    @pytest.mark.unit
+    def test_flask_login_properties(self, testclient):
+        """
+        GIVEN a Flask application configured for in this test file for testing via a fixture
+        WHEN creating a User instance in the app context and commiting to datbase with email and password
+        THEN the new user object will have and id property == 1
+        """
 
-    # Example: Insert a user into the database
-    user_email = 'user@example.com'
-    user_password = 'password'
-    user = User(email=user_email, password=user_password)
+        # Example: Insert a user into the database
+        with testclient.application.app_context():
+            user_email = 'user@example.com'
+            user_password = 'password'
+            user = User(email=user_email, password=user_password)
 
-    db.session.add(user)
-    db.session.commit()
-    user = User.query.filter_by(email='user@example.com').first()
-    user.login(user_password)
-    login_user(user)
-    assert user.id == 1
-    assert user.is_authenticated == True
+            assert user.is_authenticated == True
+            assert user.is_active == True
+            assert user.is_anonymous == False
+            # for the flask_login, i think they want the username
+            assert user.get_id() == 'user@example.com'
 
 
-@pytest.mark.auth
-@pytest.mark.users
-@pytest.mark.unit
-def test_flask_login_logout_user(testclient):
-    """
-    GIVEN a Flask application configured for in this test file for testing via a fixture
-    WHEN creating a User instance in the app context and commiting to datbase with email and password
-    THEN the new user object will have and id property == 1
-    """
 
-    # Example: Insert a user into the database
-    user_email = 'user@example.com'
-    user_password = 'password'
-    user = User(email=user_email, password=user_password)
 
-    db.session.add(user)
-    db.session.commit()
-    user = User.query.filter_by(email='user@example.com').first()
-    user.login(user_password)
-    login_user(user)
-    logout_user()
-    assert current_user.is_authenticated == False
+    @pytest.mark.auth
+    @pytest.mark.users
+    @pytest.mark.unit
+    def test_flask_login_properties_database_committed(self, testclient):
+        """
+        GIVEN a Flask application configured for in this test file for testing via a fixture
+        WHEN creating a User instance in the app context and commiting to datbase with email and password
+        THEN the new user object will have and id property == 1
+        """
+
+        # Example: Insert a user into the database
+        user_email = 'user@example.com'
+        user_password = 'password'
+        user = User(email=user_email, password=user_password)
+
+        db.session.add(user)
+        db.session.commit()
+        user = User.query.filter_by(email='user@example.com').first()
+        user.login(user_password)
+        login_user(user)
+        assert current_user.is_authenticated == True
+        assert current_user.email == 'user@example.com'
+
+
+
+    @pytest.mark.auth
+    @pytest.mark.users
+    @pytest.mark.unit
+    def test_flask_login_logout_user(self, testclient):
+        """
+        GIVEN a Flask application configured for in this test file for testing via a fixture
+        WHEN creating a User instance in the app context and commiting to datbase with email and password
+        THEN the new user object will have and id property == 1
+        """
+
+        # Example: Insert a user into the database
+        user_email = 'user@example.com'
+        user_password = 'password'
+        user = User(email=user_email, password=user_password)
+
+        db.session.add(user)
+        db.session.commit()
+        user = User.query.filter_by(email='user@example.com').first()
+        user.login(user_password)
+        login_user(user)
+        logout_user()
+        assert current_user.is_authenticated == False
 
