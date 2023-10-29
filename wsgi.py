cd85c91ad2206f2921bfb3518472298a852ded30
beau kinstler <beaukinstler@gmail.com>
2023-10-29 16:20:57 -0500

    WIP

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,15 @@
 from src import create_app
 from flask_migrate import upgrade
+from sqlalchemy.exc import ProgrammingError
 
 
 app = create_app()
 
 with app.app_context():
-    upgrade()
+    try:
+        upgrade()
+    except ProgrammingError as e:
+        print(e)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001)
