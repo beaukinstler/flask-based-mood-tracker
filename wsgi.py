17f10ea64f482fe4932f4f181cfc2b733ff3740c
Beau <git@beaukinstler.info>
2023-11-19 16:24:31 -0600

    remove unhelpful upgrade

--- wsgi.py
+++ wsgi.py
@@ -5,11 +5,7 @@ from sqlalchemy.exc import ProgrammingError
 
 app = create_app()
 
-with app.app_context():
-    try:
-        upgrade()
-    except ProgrammingError as e:
-        print(e)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001)
