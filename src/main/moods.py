146714e1c36542f08e43ac68d1438a1844207e05
= <=>
2024-02-26 06:44:51 +0000

    change: added option to add more moods

--- src/main/moods.py
+++ src/main/moods.py
@@ -13,18 +13,69 @@ import json
 
 bp = Blueprint("moods", __name__, url_prefix="/moods")
 
+mood_colors = {
+    'happy': 'primary',
+    'sad': 'warning',
+    'angry': 'danger',
+    'anxious': 'secondary',
+    'calm': 'success',
+    'confused': 'secondary',
+    'depressed': 'dark',
+    'disappointed': 'dark',
+    'excited': 'primary',
+    'frustrated': 'danger',
+    'grateful': 'success',
+    'guilty': 'secondary',
+    'hopeful': 'success',
+    'hurt': 'danger',
+    'lonely': 'dark',
+    'loved': 'success',
+    'nervous': 'secondary',
+    'overwhelmed': 'dark',
+    'peaceful': 'success',
+    'relieved': 'success',
+    'satisfied': 'primary',
+    'scared': 'secondary',
+    'shocked': 'danger',
+    'stressed': 'danger',
+    'tired': 'dark',
+    'upset': 'danger',
+    'worried': 'secondary'
+}
+
 
 class MoodForm(FlaskForm):
     note = TextAreaField('note', validators=[Length(min=0, max=240)], render_kw={
-                         'placeholder': '[Optional] Leave a note...'})
-    happy = SubmitField('Happy', id=5, render_kw={
-                        'class': 'btn btn-primary'}, )
-    sad = SubmitField('Sad', id=2, render_kw={'class': 'btn btn-warning'})
+                         'placeholder': '[Optional] Leave a note...'})              
+
+class MoodCreateForm(FlaskForm):
+    description = TextAreaField('description', validators=[Length(min=0, max=240)], render_kw={
+                         'placeholder': 'Enter a new mood type...'})
+    create = SubmitField('Create', render_kw={'class': 'btn btn-primary'})
 
 
 @bp.route("", methods=['GET', 'POST'])
 @login_required
 def index():
+    moods = db.session.execute(
+        select(Mood).order_by(Mood.id.asc())).scalars().all()
+    base_moods = db.session.execute(
+        select(Mood).where(Mood.description.in_(['happy', 'sad']))).scalars().all()
+    
+    # check for minimum moods
+    if not len(base_moods) == 2:
+        try:
+            happy = Mood('happy')
+            sad = Mood('sad')
+            db.session.add(happy)
+            db.session.add(sad)
+            db.session.commit()
+            moods = db.session.execute(
+                select(Mood).order_by(Mood.id.asc())).scalars().all()
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            abort(400, description="No mood types found and unable to create them")
     form = MoodForm()
 
     if flask_request.method == 'POST':
@@ -36,19 +87,7 @@ def index():
         moods = db.session.execute(
             select(Mood).order_by(Mood.id.asc())).scalars().all()
         # ensure we have at least 2 moods, happy and sad
-        if moods == []:
-            try:
-                happy = Mood('happy')
-                sad = Mood('sad')
-                db.session.add(happy)
-                db.session.add(sad)
-                db.session.commit()
-                moods = db.session.execute(
-                    select(Mood).order_by(Mood.id.asc())).scalars().all()
-            except Exception as e:
-                print(e)
-                db.session.rollback()
-                abort(400, description="No mood types found and unable to create them")
+        
         mood_name = [field for field in flask_request.form if field in [
             m.description for m in moods]].pop()
         mood = [mood for mood in moods if mood.description in flask_request.form].pop()
@@ -72,7 +111,9 @@ def index():
                     flash(
                         f'There was a problem logging your {mood.description} mood...')
         return flask_redirect(url_for('users.me'))
-    return flask_render_template('mood.html', form=form)
+    for m in moods:
+        setattr(m, 'color', mood_colors[m.description] if m.description in mood_colors else 'secondary')
+    return flask_render_template('mood.html', form=form, moods=moods)
 
 
 @bp.route("/all", methods=['GET'])
@@ -85,19 +126,28 @@ def all():
     return flask_render_template('mood_list.html', moods=moods)
 
 
-@bp.route("/create", methods=['POST'])
+@bp.route("/create", methods=['GET','POST'])
 @login_required
 def create():
-    if 'description' not in flask_request.json:
-        return abort(400)
-    mood = Mood(flask_request.json['description'])
-    try:
-        db.session.add(mood)
-        db.session.commit()
-    except IntegrityError as e:
-        db.session.rollback()
-        abort(409, description="Attempted Data Duplicate or other Integrity Error")
-    return jsonify(mood.serialize())
+    form = MoodCreateForm()
+    if flask_request.method == 'POST':
+        if form.validate():
+            pass
+        else:
+            flash('Invalid data')
+            return flask_redirect(url_for('moods.create'))
+        if 'description' not in flask_request.form:
+            return abort(400, description="We had trouble with the form data. Please try again.")
+        mood = Mood(flask_request.form['description'])
+        try:
+            db.session.add(mood)
+            db.session.commit()
+        except IntegrityError as e:
+            db.session.rollback()
+            abort(409, description="Attempted Data Duplicate or other Integrity Error")
+        flash(f'Created a new mood type: {mood.description}')
+        return flask_redirect(url_for('moods.all'))
+    return flask_render_template('mood_create.html', form=form)
 
 
 @bp.route("/<int:id>", methods=['GET'])
